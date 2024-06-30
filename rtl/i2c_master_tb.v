657fbdc61dc86cd9c5db6b5ba2e22abbb812715e
Kreijstal <rainb@tfwno.gf>
2024-06-30 11:42:38 +0200

    generated blocks work

--- rtl/i2c_master_tb.v
+++ rtl/i2c_master_tb.v
@@ -10,11 +10,14 @@ module i2c_master_tb;
   // Parameters
 
   parameter CLK_PERIOD = 10;  // 10ns clock period (100MHz clock)
-  // Inputs
-  reg clk = 0;
+    parameter ENABLE_DEVICE_3 = 1;  // Set to 0 to disable device 3
+  parameter ENABLE_DEVICE_4 = 1;  // Set to 0 to disable device 4
+
+    reg clk = 0;
   reg rst = 0;
   reg [7:0] current_test = 0;
 
+  // I2C master signals
   reg [6:0] s_axis_cmd_address = 0;
   reg s_axis_cmd_start = 0;
   reg s_axis_cmd_read = 0;
@@ -31,7 +34,6 @@ module i2c_master_tb;
   reg [15:0] prescale = 0;
   reg stop_on_idle = 0;
 
-  // Outputs
   wire s_axis_cmd_ready;
   wire s_axis_data_tready;
   wire [7:0] m_axis_data_tdata;
@@ -46,29 +48,111 @@ module i2c_master_tb;
   wire bus_active;
   wire missed_ack;
 
-  // Additional wires for modeling pull-up resistors and open-drain outputs
+  // Wires for modeling pull-up resistors and open-drain outputs
   wire scl_wire;
   wire sda_wire;
 
-  // Existing dummy registers
+  // Dummy registers
   reg sda2 = 1;
   reg scl2 = 1;
 
-  // New device (register) signals
-  wire sda_o_3;
-  //reg sda_o_3=1;
-  wire sda_t_3;
-  wire scl_o_3;
-  //reg scl_o_3=1;
-  wire scl_t_3;
+  // Generate block for Device 3
+  generate
+    if (ENABLE_DEVICE_3) begin : device_3
+      wire sda_o_3;
+      wire sda_t_3;
+      wire scl_o_3;
+      wire scl_t_3;
+      reg [7:0] data_in_3;
+      reg data_latch_3;
+      wire [7:0] data_out_3;
+
+      i2c_single_reg #(
+          .FILTER_LEN(4),
+          .DEV_ADDR  (7'h70)
+      ) i2c_reg (
+          .clk(clk),
+          .rst(rst),
+          .scl_i(scl_wire),
+          .scl_o(scl_o_3),
+          .scl_t(scl_t_3),
+          .sda_i(sda_wire),
+          .sda_o(sda_o_3),
+          .sda_t(sda_t_3),
+          .data_in(data_in_3),
+          .data_latch(data_latch_3),
+          .data_out(data_out_3)
+      );
+    end
+  endgenerate
+
+  // Generate block for Device 4
+  generate
+    if (ENABLE_DEVICE_4) begin : device_4
+      wire sda_o_4;
+      wire sda_t_4;
+      wire scl_o_4;
+      wire scl_t_4;
+      wire [7:0] m_axis_data_tdata_4;
+      wire m_axis_data_tvalid_4;
+      wire m_axis_data_tready_4;
+      wire m_axis_data_tlast_4;
+      wire [7:0] s_axis_data_tdata_4;
+      wire s_axis_data_tvalid_4;
+      wire s_axis_data_tready_4;
+      wire s_axis_data_tlast_4;
+      wire busy_4;
+      wire [6:0] bus_address_4;
+      wire bus_addressed_4;
+      wire bus_active_4;
+      reg release_bus_4;
+      reg enable_4;
+      reg [6:0] device_address_4;
+      reg [6:0] device_address_mask_4;
+
+      i2c_slave #(
+          .FILTER_LEN(4)
+      ) i2c_slave_inst (
+          .clk(clk),
+          .rst(rst),
+          .release_bus(release_bus_4),
+          .s_axis_data_tdata(s_axis_data_tdata_4),
+          .s_axis_data_tvalid(s_axis_data_tvalid_4),
+          .s_axis_data_tready(s_axis_data_tready_4),
+          .s_axis_data_tlast(s_axis_data_tlast_4),
+          .m_axis_data_tdata(m_axis_data_tdata_4),
+          .m_axis_data_tvalid(m_axis_data_tvalid_4),
+          .m_axis_data_tready(m_axis_data_tready_4),
+          .m_axis_data_tlast(m_axis_data_tlast_4),
+          .scl_i(scl_wire),
+          .scl_o(scl_o_4),
+          .scl_t(scl_t_4),
+          .sda_i(sda_wire),
+          .sda_o(sda_o_4),
+          .sda_t(sda_t_4),
+          .busy(busy_4),
+          .bus_address(bus_address_4),
+          .bus_addressed(bus_addressed_4),
+          .bus_active(bus_active_4),
+          .enable(enable_4),
+          .device_address(device_address_4),
+          .device_address_mask(device_address_mask_4)
+      );
+    end
+  endgenerate
 
   // Model pull-up resistors with weak pull-ups
   pullup (scl_wire);
   pullup (sda_wire);
 
-  // Model open-drain outputs, including the new device
-  assign scl_wire = (scl_o & scl2 & scl_o_3) ? 1'bz : 1'b0;
-  assign sda_wire = (sda_o & sda2 & sda_o_3) ? 1'bz : 1'b0;
+  // Model open-drain outputs, including conditional devices
+  assign scl_wire = (scl_o & scl2 & 
+                     (ENABLE_DEVICE_3 ? device_3.scl_o_3 : 1'b1) & 
+                     (ENABLE_DEVICE_4 ? device_4.scl_o_4 : 1'b1)) ? 1'bz : 1'b0;
+  assign sda_wire = (sda_o & sda2 & 
+                     (ENABLE_DEVICE_3 ? device_3.sda_o_3 : 1'b1) & 
+                     (ENABLE_DEVICE_4 ? device_4.sda_o_4 : 1'b1)) ? 1'bz : 1'b0;
+
 
   always #(CLK_PERIOD / 2) clk <= ~clk;
 
@@ -92,27 +176,7 @@ module i2c_master_tb;
     $dumpvars(0, i2c_master_tb);
 
   end
-  reg [7:0] data_in_3;
-  reg data_latch_3;
-  wire [7:0] data_out_3;
-  i2c_single_reg #(
-      .FILTER_LEN(4),
-      .DEV_ADDR  (7'h70)
-  ) i2c_reg (
-      .clk(clk),
-      .rst(rst),
 
-      .scl_i(scl_wire),
-      .scl_o(scl_o_3),
-      .scl_t(scl_t_3),
-      .sda_i(sda_wire),
-      .sda_o(sda_o_3),
-      .sda_t(sda_t_3),
-
-      .data_in(data_in_3),
-      .data_latch(data_latch_3),
-      .data_out(data_out_3)
-  );
   i2c_master UUT (
       .clk(clk),
       .rst(rst),
@@ -245,7 +309,8 @@ task test_writing;
         repeat(7) @(negedge scl_o);
         send_ack();
         @(negedge scl_o);
-        //send_byte(8'b01111111);
+         //send_byte(8'b01111111);
+        wait_for_ready();
        end
 endtask
 
@@ -286,8 +351,8 @@ task test_i2c_single_reg_writing;
         s_axis_data_tdata = 8'd55;
         s_axis_data_tvalid = 1;
         wait_for_ready();
-        if (data_out_3 != 8'd55) $fatal(1, "We didn't get what we sent");
-        $display("Received data %d", data_out_3);
+        if (device_3.data_out_3 != 8'd55) $fatal(1, "We didn't get what we sent");
+        $display("Received data %d", device_3.data_out_3);
     end
 endtask
 
@@ -295,10 +360,10 @@ endtask
 task test_i2c_single_reg_reading;
     begin
         $display("Testing i2c_single_reg reading");
-        data_latch_3 = 1;
-        data_in_3 = 8'd123;
+        device_3.data_latch_3 = 1;
+        device_3.data_in_3 = 8'd123;
         #(CLK_PERIOD);
-        data_latch_3 = 0;
+        device_3.data_latch_3 = 0;
         i2c_start(7'h70, 1);
         @(posedge m_axis_data_tvalid);
         $display("Received m_axis_data_tdata %d", m_axis_data_tdata);
@@ -310,10 +375,9 @@ endtask
     $display("Starting I2C Master test");
     initialize_testbench;
     test_nack_handling();
-     stop_on_idle = 1;
+    stop_on_idle = 1;
     test_writing();
-        wait_for_ready();
-        #1000;
+    #1000;//stopping
 
     test_reading();
     wait_for_ready();
